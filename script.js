70974f2095d82c029034f611d4971004e93c7cd0
elleagn <133775323+elleagn@users.noreply.github.com>
2023-06-14 00:22:53 +0200

    Add color and rid size selection
    Add a slider for grid size selection and a color picker for color
    selection.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const createGrid = function () {
+const createGrid = function (size) {
   const container = document.querySelector(".container");
-  for (let i = 0; i < 16; i++) {
+  for (let i = 0; i < size; i++) {
     const row = document.createElement("div");
     row.classList.add("row");
-    for (j = 0; j < 16; j++) {
+    for (j = 0; j < size; j++) {
       const box = document.createElement("div");
       box.classList.add("box");
       row.appendChild(box);
@@ -12,16 +12,47 @@ const createGrid = function () {
   }
 };
 
-const colorOnHover = function () {
+const colorOnHover = function (color) {
   const boxes = document.querySelectorAll(".box");
   boxes.forEach((box) => {
     box.addEventListener("mouseover", () => {
-      box.style.backgroundColor = "black";
+      box.style.backgroundColor = color;
     });
   });
 };
 
-const setupThePage = function {
-    createGrid();
-    colorOnHover();
-}
+const handleRangeChange = function (event) {
+  const grid = document.querySelector(".container");
+  const rows = document.querySelectorAll(".row");
+  rows.forEach((row) => {
+    grid.removeChild(row);
+  });
+  const range = event.target;
+  const size = range.valueAsNumber;
+  createGrid(size);
+  colorOnHover();
+  document.querySelector("output").textContent = `${size}`;
+};
+
+const changeTheColor = function (event) {
+  const color = event.target;
+  colorOnHover(color.value);
+};
+const setDefault = function () {
+  const size = document.querySelector("input[type=range]");
+  size.value = 16;
+  const color = document.querySelector("input[type=color]");
+  color.value = "#f5f5f5";
+};
+
+const setupThePage = function () {
+  setDefault();
+  createGrid(16);
+  colorOnHover("whitesmoke");
+  const size = document.querySelector("input[type=range]");
+  size.addEventListener("input", handleRangeChange);
+  const color = document.querySelector("input[type=color]");
+  color.addEventListener("input", changeTheColor);
+};
+
+setupThePage();
